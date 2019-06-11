6bda374472dffb23e0e13d65df103cf6b3b58306
Unknown <arthur.simonian@outlook.fr>
2019-06-11 23:19:34 +0200

    relation user and task in task controller
    modfication user and task entities + task controller

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Table("user")
@@ -38,7 +39,16 @@ class User implements UserInterface
      * @Assert\Email(message="Le format de l'adresse n'est pas correcte.")
      */
     private $email;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Task", mappedBy="task")
+     */
+    private $tasks;
 
+    public function __construct(){
+       $this->tasks = new ArrayCollection();
+    }
+        
     public function getId()
     {
         return $this->id;
@@ -86,5 +96,29 @@ class User implements UserInterface
 
     public function eraseCredentials()
     {
+    }
+    
+        public function getTasks(): Collection
+    {
+        return $this->tasks;
+    }
+    public function addTask(Task $task): self
+    {
+        if (!$this->tasks->contains($task)) {
+            $this->tasks[] = $task;
+            $task->setUser($this);
+        }
+        return $this;
+    }
+    public function removeTask(Task $task): self
+    {
+        if ($this->tasks->contains($task)) {
+            $this->tasks->removeElement($task);
+
+            if ($task->getUser() === $this) {
+                $task->setUser(null);
+            }
+        }
+        return $this;
     }
 }
